f5109e853847feb99ceea9d4e08c9aec6acdb3ae
Web Gestion <webgestion95@gmail.com>
2026-06-06 14:43:49 +0200

    Updated files vite and package

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,18 @@
 //     componentTagger (dev-only), VITE_* env injection, @ path alias, React/TanStack dedupe,
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
 
-   vite: {
-    build: {
-      ssr: false
-    },
+  plugins: [react()],
+  build: {
+    ssr: false,
+    outDir: "dist/client"
+  },
 
   server: {
     allowedHosts: true
   }
-}
 });
