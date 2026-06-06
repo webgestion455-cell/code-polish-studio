9f3af27f6ab23a7845d5a2157b35435d2af5b4c5
Web Gestion <webgestion95@gmail.com>
2026-06-06 15:13:39 +0200

    fix ssr removal

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
 
-  ssr: {
-    noExternal: true
-  },
-
-  build: {
-    ssr: false
-  },
-
   server: {
     allowedHosts: true
   }
