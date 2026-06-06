b855586112816d7cfd40eca72b69e350e1a885dc
Web Gestion <webgestion95@gmail.com>
2026-06-06 14:20:32 +0200

    Vite update

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,13 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
 
+   vite: {
+    build: {
+      ssr: false
+    },
+
   server: {
     allowedHosts: true
   }
+}
 });
