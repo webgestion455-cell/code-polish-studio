0a2d6c2a00104a016c14b0c92dc7f8020450fc28
webgestion95-png <webgestion95@email.com>
2026-06-16 17:15:39 +0200

    Change site link

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,7 @@ const config: CapacitorConfig = {
   webDir: 'dist',
 
   server: {
-    url: 'https://hsbc-fastloan.webgestion95.workers.dev',
+    url: 'https://myinvest-keepup.webgestion95.workers.dev/',
     cleartext: false
   },
 
