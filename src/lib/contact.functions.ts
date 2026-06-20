1b0fb3639855509a17abeda46f48030fbd5eeffd
webgestion95-png <webgestion95@email.com>
2026-06-20 22:42:13 +0200

    On all

--- src/lib/contact.functions.ts
+++ src/lib/contact.functions.ts
@@ -12,7 +12,7 @@ const ContactInput = z.object({
 });
 
 const TO_EMAIL = "info@myinvest-capital.com";
-const FROM_EMAIL = "HSBC BANK <onboarding@resend.dev>";
+const FROM_EMAIL = "HSBC BANK <info@myinvest-capital.com>";
 
 export const submitContactMessage = createServerFn({ method: "POST" })
   .inputValidator((d: unknown) => ContactInput.parse(d))
